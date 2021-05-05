e5978a4010720c686bca8ad89c3cb277ede1364f
wbsnail <wbsnail@pm.me>
2021-05-06 02:01:26 +0800

    4-informer

---
+++ archive/kubernetes-informer-source-code/4-informer/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+func main() {
+	lw := newConfigMapsListerWatcher()
+	// 第一个返回的参数是 cache.Store，这里用不到所以直接丢弃
+	_, controller := cache.NewInformer(lw, &corev1.ConfigMap{}, 0, cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			configMap, ok := obj.(*corev1.ConfigMap)
+			if !ok {
+				return
+			}
+			fmt.Printf("created: %s\n", configMap.Name)
+		},
+		UpdateFunc: func(old, new interface{}) {
+			configMap, ok := old.(*corev1.ConfigMap)
+			if !ok {
+				return
+			}
+			fmt.Printf("updated: %s\n", configMap.Name)
+		},
+		DeleteFunc: func(obj interface{}) {
+			configMap, ok := obj.(*corev1.ConfigMap)
+			if !ok {
+				return
+			}
+			fmt.Printf("deleted: %s\n", configMap.Name)
+		},
+	})
+
+	stopper := make(chan struct{})
+	defer close(stopper)
+
+	fmt.Println("Start syncing....")
+
+	go controller.Run(stopper)
+
+	<-stopper
+}
