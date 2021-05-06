d2bdf35b59747daf8c8f51282b92a9bfc3238490
wbsnail <wbsnail@pm.me>
2021-05-07 01:51:37 +0800

    Print demo name

--- archive/dive-into-kubernetes-informer/9-shared-informer-factory/main.go
+++ archive/dive-into-kubernetes-informer/9-shared-informer-factory/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	fmt.Println("----- 9-shared-informer-factory -----")
+
 	// mustClientset 用于创建 kubernetes.Interface 实例；
 	// 第 2 个参数是 defaultResync，是构建新 Informer 时默认的 resyncPeriod，
 	// resyncPeriod 在前一部分中介绍过了；
