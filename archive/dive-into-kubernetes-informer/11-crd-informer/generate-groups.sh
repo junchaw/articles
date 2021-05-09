93e5e90cb31c15fba1e15269cb318632ae95cafc
wbsnail <wbsnail@pm.me>
2021-05-10 00:03:38 +0800

    SCRIPT_PATH

--- archive/dive-into-kubernetes-informer/11-crd-informer/generate-groups.sh
+++ archive/dive-into-kubernetes-informer/11-crd-informer/generate-groups.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-~/projects/code-generator/generate-groups.sh \
+scriptPath=${SCRIPT_PATH}
+
+if [ -z ${scriptPath} ]; then
+  echo "Tell me, where is the file? (SCRIPT_PATH)"
+  echo "Maybe 'export SCRIPT_PATH=~/projects/code-generator/generate-groups.sh'?"
+  exit 1
+fi
+
+${scriptPath} \
   all \
   github.com/wbsnail/articles/archive/dive-into-kubernetes-informer/11-crd-informer/client \
   github.com/wbsnail/articles/archive/dive-into-kubernetes-informer/11-crd-informer/api \
